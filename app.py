3d71aca66c63407f7e9ca19160066da6f4024bff
Ricardo Martinez <rbeltra1@mycambrian.ca>
2025-05-23 17:22:37 -0400

    Second class

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
     return "<h1>Hello World<h1>"
+
+@app.route('/hello')
+def hello():
+    return "Hello world"
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello {name}"
+
+@ app.route('/add/<int:number1>/<int:number2>')
+def add(number1, number2):
+    return f"{number1}+{number2}={number1+number2}"
+
+@app.route('/handle_url_params')
+def handle_params():
+    return str(request.args)
+
 
 if __name__=='__main__':
     app.run(host='127.0.0.1', debug=True)
