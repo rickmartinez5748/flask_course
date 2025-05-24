49b7acad3afc06373c2b000e4563915c667d474b
Ricardo Martinez <rbeltra1@mycambrian.ca>
2025-05-23 20:44:16 -0400

    Third class part A, minuto 41:57

--- app.py
+++ app.py
@@ -1,27 +1,14 @@
-from flask import Flask, request
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app=Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    return "<h1>Hello World<h1>"
-
-@app.route('/hello')
-def hello():
-    return "Hello world"
-
-@app.route('/greet/<name>')
-def greet(name):
-    return f"Hello {name}"
-
-@ app.route('/add/<int:number1>/<int:number2>')
-def add(number1, number2):
-    return f"{number1}+{number2}={number1+number2}"
-
-@app.route('/handle_url_params')
-def handle_params():
-    return str(request.args)
-
+    myvalue="Ricardo"
+    myage=37
+    
+    mylist=[10,20,30,40,50]
+    return render_template("index.html", myvalue=myvalue, myage=myage, mylist=mylist)
 
 if __name__=='__main__':
     app.run(host='127.0.0.1', debug=True)
